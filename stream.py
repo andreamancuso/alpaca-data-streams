84193bc7434eb18e9d7f9838e67f27e9dbacea40
Andrea Mancuso <and.mancuso@googlemail.com>
2024-09-23 20:10:16 +0200

    Update __init__.py, poetry.lock, and 3 more files...

--- stream.py
+++ stream.py
@@ -1,7 +1,19 @@
+import argparse
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
 from alpaca.data.live import StockDataStream
 
+parser = argparse.ArgumentParser()
+parser.add_argument('api_key')
+parser.add_argument('secret_key')
+args = parser.parse_args()
 
-wss_client = StockDataStream('', '')
+api_key = os.environ.get('API_KEY') or args.api_key
+secret_key = os.environ.get('SECRET_KEY') or args.secret_key
+
+wss_client = StockDataStream(api_key, secret_key)
 
 # async handler
 async def quote_data_handler(data):
