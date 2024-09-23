f24ba719f64a2ad527f518275a263103c7eb92a5
Andrea Mancuso <and.mancuso@googlemail.com>
2024-09-23 20:59:46 +0200

    Update poetry.lock, pyproject.toml, and stream.py

--- stream.py
+++ stream.py
@@ -1,6 +1,10 @@
 import argparse
 import os
 from dotenv import load_dotenv
+from fastapi import FastAPI, Response
+from fastapi.responses import StreamingResponse
+from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
 load_dotenv()
 
 from alpaca.data.live import StockDataStream
@@ -15,11 +19,28 @@ secret_key = os.environ.get('SECRET_KEY') or args.secret_key
 
 wss_client = StockDataStream(api_key, secret_key)
 
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 # async handler
-async def quote_data_handler(data):
+def quote_data_handler(data):
     # quote data will arrive here
-    print(data)
+    yield data
 
 wss_client.subscribe_quotes(quote_data_handler, "SPY")
 
 wss_client.run()
+
+@app.get("/stock")
+async def root():
+    return StreamingResponse(quote_data_handler(), media_type="text/event-stream")
+
+
+uvicorn.run(app, host="127.0.0.1", port=8000)
