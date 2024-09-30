b9c38a32e4962c1ff61377ec42dfb37140434298
andrea.mancuso <andrea.mancuso@u-blox.com>
2024-09-30 12:30:52 +0200

    Update index.html, index.ts, and 2 more files...

--- index.ts
+++ index.ts
@@ -1,8 +1,7 @@
-import express, { Request, Response } from "express";
+// import express, { Request, Response } from "express";
 import { Server } from "ws";
 import dotenv from "dotenv";
 import Alpaca from "@alpacahq/alpaca-trade-api";
-import { AlpacaStocksClient } from "@alpacahq/alpaca-trade-api/dist/resources/datav2/stock_websocket_v2";
 import { AlpacaCryptoClient } from "@alpacahq/alpaca-trade-api/dist/resources/datav2/crypto_websocket_v1beta3";
 
 dotenv.config();
@@ -10,6 +9,7 @@ dotenv.config();
 const API_ENDPOINT = process.env.API_ENDPOINT as string;
 const API_KEY = process.env.API_KEY as string;
 const API_SECRET = process.env.API_SECRET as string;
+const port = process.env.PORT as string;
 
 type DataStreamOptions = {
     baseUrl: string;
@@ -49,8 +49,6 @@ class DataStream {
 }
 
 const runner = async () => {
-    console.log(API_ENDPOINT, API_KEY, API_SECRET);
-
     const stream = new DataStream({
         baseUrl: API_ENDPOINT,
         apiKey: API_KEY,
@@ -60,7 +58,7 @@ const runner = async () => {
 
     await stream.connect();
 
-    const wss = new Server({ port: 8765 });
+    const wss = new Server({ port: Number(port) });
 
     wss.on("connection", (ws) => {
         console.log("new connection established");
@@ -72,14 +70,14 @@ const runner = async () => {
 
             switch (message.action) {
                 case "subscribe": {
-                    console.log("a");
+                    // console.log("a");
                     stream.socket.subscribeForQuotes(message.ticks);
-                    console.log("b");
+                    // console.log("b");
                     stream.socket.onCryptoQuote((quote) => {
                         ws.send(JSON.stringify(quote));
-                        console.log(quote);
+                        // console.log(quote);
                     });
-                    console.log("c");
+                    // console.log("c");
 
                     /**
                      * {
@@ -103,20 +101,18 @@ const runner = async () => {
         console.log("connection closed");
     });
 
-    const app = express();
+    // const app = express();
 
-    const port = process.env.PORT as string;
+    // app.get("/", (request: Request, response: Response) => {
+    //     response.status(200).send("Hello World");
+    // });
 
-    app.get("/", (request: Request, response: Response) => {
-        response.status(200).send("Hello World");
-    });
-
-    app.listen(port, () => {
-        console.log("Server running at PORT: ", port);
-    }).on("error", (error) => {
-        // gracefully handle error
-        throw new Error(error.message);
-    });
+    // app.listen(port, () => {
+    //     console.log("Server running at PORT: ", port);
+    // }).on("error", (error) => {
+    //     // gracefully handle error
+    //     throw new Error(error.message);
+    // });
 };
 
 runner();
